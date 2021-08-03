4668fd003cabb0fc76d289b8d442caa77b24a4ea
Jose Lopez <joselopez11394@gmail.com>
2021-08-03 01:07:51 -0400

    adds update method to controller to change the ace value

--- app/controllers/api/v1/game_actions_controller.rb
+++ app/controllers/api/v1/game_actions_controller.rb
@@ -6,4 +6,16 @@ class Api::V1::GameActionsController < ApplicationController
   def stand
     redirect_to game_path(data: GameFacade.stand( current_user.username, params["player_hand"], params["dealer_hand"], params["deck"]) )
   end
+
+  def update
+    # require 'pry'; binding.pry
+    card = Card.find_by(id: params[:id])
+    if card.number == 11
+      card.update(number: 1)
+    elsif card.number == 1
+      card.update(number: 11)
+    end
+
+    redirect_to game_path(data: {player_hand: params[:player_hand], dealer_hand: params[:dealer_hand], deck: params[:deck]})
+  end
 end
