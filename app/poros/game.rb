17cd677e79693d0c471d46c1c44564caf66e8608
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-08-02 20:52:00 -0400

    Created the game Poro

--- app/poros/game.rb
+++ app/poros/game.rb
@@ -1,11 +1,17 @@
 class Game
     attr_accessor :player, :dealer, :deck
-    def initialize(username)
-        @deck = Card.get_shuffled_deck
-        @player = Player.new(username, [@deck[-1], @deck[-3]])
-        @dealer = Player.new('dealer', [@deck[-2], @deck[-4]])
-        
-        # Removed the last 4 cards
-        @deck.pop(4)
+    def initialize(username, data = nil)
+        if data == nil
+            @deck = Card.get_shuffled_deck
+            @player = Player.new(username, [@deck[-1], @deck[-3]])
+            @dealer = Player.new('Dealer', [@deck[-2], @deck[-4]])
+            
+            # Removed the last 4 cards
+            @deck.pop(4)
+        else
+            @deck = data[:deck]
+            @player = Player.new(username, Card.find(data[:player_hand]))
+            @dealer = Player.new('Dealer', Card.find(data[:dealer_hand]))
+        end
     end
 end
