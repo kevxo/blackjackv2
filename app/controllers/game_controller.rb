97d630a02eacd6b8114316362d2ca18eb7f5fa83
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-08-02 22:48:12 -0400

    created if statements for different outcomes

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,11 @@
 class GameController < ApplicationController
     def index
-        if params[:data]
+        if params[:data] && params[:data][:winner]
+            @game = Game.new(current_user.username, params[:data], params[:data][:winner])
+        elsif params[:data]
             @game = Game.new(current_user.username, params[:data])
+        elsif params[:play_again]
+            @game = Game.new(current_user.username, params[:deck])
         else
             @game = Game.new(current_user.username)
         end
