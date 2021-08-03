fa25d45726e69b114e548da2b0f9868a00bb9591
Roberto Basulto <Roberto.Basulto085@gmail.com>
2021-08-02 22:48:40 -0400

    Finnished the stand controller path

--- app/controllers/api/v1/game_actions_controller.rb
+++ app/controllers/api/v1/game_actions_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::GameActionsController < ApplicationController
   def create
-    redirect_to game_path(data: GameFacade.hit(current_user.username, params["player_hand"], params["dealer_hand"], params["deck"]))
+    redirect_to game_path(data: GameFacade.hit( current_user.username, params["player_hand"], params["dealer_hand"], params["deck"]) )
   end
 
   def stand
-    redirect_to game_path(data:)
+    redirect_to game_path(data: GameFacade.stand( current_user.username, params["player_hand"], params["dealer_hand"], params["deck"]) )
   end
 end
